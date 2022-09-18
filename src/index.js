9545c2e086faaae94a82afe3cda751a1f1d09af3
mariovasquez <N00156666@upn.pe>
2022-09-18 18:00:36 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/css/index.css';
+import './styles/css/library.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
